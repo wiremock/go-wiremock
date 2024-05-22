ff2cf596ce4b4070de6af84b244691b505e511df
Sergey Otryvanov <sergey.otryvanov@sbermarket.ru>
2024-05-22 12:02:32 +0300

    add method get, remove and find requests

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ import (
 const (
 	wiremockAdminURN         = "__admin"
 	wiremockAdminMappingsURN = "__admin/mappings"
+	wiremockAdminRequestsURN = "__admin/requests"
 )
 
 // A Client implements requests to the wiremock server.
@@ -181,4 +182,153 @@ func (c *Client) DeleteStubByID(id string) error {
 // DeleteStub deletes stub mapping.
 func (c *Client) DeleteStub(s *StubRule) error {
 	return c.DeleteStubByID(s.UUID())
+}
+
+func (c *Client) GetAllServeEvents() (*ServeEvent, error) {
+	res, err := http.Get(fmt.Sprintf("%s/%s", c.url, wiremockAdminRequestsURN))
+	if err != nil {
+		return nil, fmt.Errorf("get all requests: %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("get all requests: read response error: %s", err.Error())
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get all requests: bad response status: %d, response: %s", res.StatusCode, string(bodyBytes))
+	}
+
+	var response ServeEvent
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		return nil, fmt.Errorf("get all requests: error unmarshalling response: %s", err.Error())
+	}
+	return &response, nil
+}
+
+func (c *Client) DeleteAllRequests() error {
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/%s", c.url, wiremockAdminRequestsURN), nil)
+	if err != nil {
+		return fmt.Errorf("delete all requests: build error: %s", err.Error())
+	}
+	res, err := (&http.Client{}).Do(req)
+
+	if err != nil {
+		return fmt.Errorf("delete all requests: request error: %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return fmt.Errorf("read response error: %s", err.Error())
+		}
+		return fmt.Errorf("bad response status: %d, response: %s", res.StatusCode, string(bodyBytes))
+	}
+	return nil
+}
+
+func (c *Client) GetRequestsById(requestId string) (*RequestElement, error) {
+	res, err := http.Get(fmt.Sprintf("%s/%s/%s", c.url, wiremockAdminRequestsURN, requestId))
+	if err != nil {
+		return nil, fmt.Errorf("get request by id: build request error: %s", err.Error())
+	}
+	defer res.Body.Close()
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("get request by id: read response error: %s", err.Error())
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get request by id: bad response status: %d, response: %s", res.StatusCode, string(bodyBytes))
+	}
+
+	var response RequestElement
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		return nil, fmt.Errorf("get request by id: error unmarshalling response: %s", err.Error())
+	}
+	return &response, nil
+}
+
+func (c *Client) DeleteRequestById(requestId string) error {
+	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("%s/%s/%s", c.url, wiremockAdminRequestsURN, requestId), nil)
+	if err != nil {
+		return fmt.Errorf("delete request by id: build request error: %s", err.Error())
+	}
+
+	res, err := (&http.Client{}).Do(req)
+	if err != nil {
+		return fmt.Errorf("delete request by id: request error: %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return fmt.Errorf("delete request by id: read response error: %s", err.Error())
+		}
+		return fmt.Errorf("delete request by id: bad response status: %d, response: %s", res.StatusCode, string(bodyBytes))
+	}
+	return nil
+}
+
+func (c *Client) DeleteRequestBy(r *Request) (*ServeEvents, error) {
+	requestBody, err := r.MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("delete request by: build error: %s", err.Error())
+	}
+
+	res, err := http.Post(fmt.Sprintf("%s/%s/remove", c.url, wiremockAdminRequestsURN), "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, fmt.Errorf("delete request by: request error: %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("delete request by: read response error: %s", err.Error())
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("delete request by: bad response status: %d, response: %s", res.StatusCode, string(bodyBytes))
+	}
+
+	var serveEvents ServeEvents
+	err = json.Unmarshal(bodyBytes, &serveEvents)
+	if err != nil {
+		return nil, fmt.Errorf("get request by id: error unmarshalling response: %s", err.Error())
+	}
+	return &serveEvents, nil
+}
+
+func (c *Client) FindRequestBy(r *Request) (*ServeEvents, error) {
+	requestBody, err := r.MarshalJSON()
+	if err != nil {
+		return nil, fmt.Errorf("find request by: build error: %s", err.Error())
+	}
+
+	res, err := http.Post(fmt.Sprintf("%s/%s/remove", c.url, wiremockAdminRequestsURN), "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, fmt.Errorf("find request by: request error: %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("get count requests: read response error: %s", err.Error())
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get count requests: bad response status: %d, response: %s", res.StatusCode, string(bodyBytes))
+	}
+
+	var serveEvents ServeEvents
+	err = json.Unmarshal(bodyBytes, &serveEvents)
+	if err != nil {
+		return nil, fmt.Errorf("get count requests: read json error: %s", err.Error())
+	}
+	return &serveEvents, nil
 }
